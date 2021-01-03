2584d36591526365639cf2a25718e55c299c9383
youngcheon <76905528+youngcheon@users.noreply.github.com>
2021-01-04 08:49:20 +0900

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,22 +5,20 @@ from bs4 import BeautifulSoup
 
 bot = telegram.Bot(token = '1426984672:AAGe3XoDWRfApd62Fd_POFuvT4iBNIRro-A')
 
-
-num = 18552
-
+lastest_num = 0
 
 while True :
-    time.sleep(10)
     request = requests.get('https://www.scnu.ac.kr/SCNU/na/ntt/selectNttList.do?mi=1196&bbsId=1074')
     html = request.text
     soup = BeautifulSoup(html, 'html.parser')
     titles = soup.select('td > a')
-    del titles[0]
-    del titles[1:]
-    if int(soup.find_all(class_ = 'BD_tm_none')[9].text) > num: #글번호가 최신이면                    
-        for title in titles:  
-            bot.sendMessage(chat_id = '-430078681', text = f"{title.text.strip()}\n\nhttps://www.scnu.ac.kr{title.attrs['href']}")  
-        num += 1
+    post_num = int(soup.find_all(class_ = 'BD_tm_none')[9].text)        
+    if lastest_num != post_num:
+        lastest_num = post_num          
+        # bot.sendMessage(chat_id = '-430078681', text = f"{titles[1].text.strip()}\n\nhttps://www.scnu.ac.kr{titles[0].attrs['href']}")  
+        bot.sendMessage(chat_id = '1497406622', text = f"{titles[1].text.strip()}\n\nhttps://www.scnu.ac.kr{titles[0].attrs['href']}")  
+        
+    time.sleep(10)
     
 
 
